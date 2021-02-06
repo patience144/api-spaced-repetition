77efa08abb8b0839a146f5dee18766cc113f5c6c
Quiana <quianalee@Quianas-MacBook-Air.local>
2021-02-05 22:38:36 -0500

    changes

--- postgrator-config.js
+++ postgrator-config.js
@@ -1,8 +1,9 @@
 require('dotenv').config();
-
+const pg = require('pg');
+pg.defaults.ssl = process.env.NODE_ENV === "production";
 module.exports = {
   "migrationDirectory": "migrations",
   "driver": "pg",
   "connectionString": process.env.DATABASE_URL,
-  "ssl": false,
+  
 }
--- src/server.js
+++ src/server.js
@@ -1,5 +1,6 @@
 require('dotenv').config()
-
+const pg = require('pg');
+pg.defaults.ssl = process.env.NODE_ENV === "production";
 const knex = require('knex')
 const app = require('./app')
 const { PORT, DATABASE_URL } = require('./config')
